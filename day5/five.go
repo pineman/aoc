fe22788c4480f757ae1ba993002e0582489a6ba6
João Pinheiro <joao.castropinheiro@gmail.com>
2022-12-06 21:34:28 +0000

    aoc2022: day 5 refactor for bigboy input

--- day5/five.go
+++ day5/five.go
@@ -6,6 +6,7 @@ import (
 	aoc2022 "github.com/pineman/code/chall/aoc2022/go"
 )
 
+// TODO: try to use a linked list, inserting/removing at the head
 type stack []byte
 
 func (s *stack) Push(v byte) {
@@ -18,68 +19,82 @@ func (s *stack) Pop() byte {
 	return res
 }
 
-// The puzzle inputs are so well-crafted that it diminishes the fun - each row
-// has trailing spaces up to the 36th column (=4 spaces each * 9 columns - 1)
-func parseCrates(input []string) [9]stack {
-	var crateColumns [9]stack
-	for col := 0; col < 9; col++ {
-		for row := 7; row >= 0; row-- {
+func getMatrixDimensions(input []string) (int, int) {
+	rows, cols := 0, (len(input[0])+1)/4
+	for i, v := range input {
+		if v[:2] == " 1" {
+			rows = i
+			break
+		}
+	}
+	return rows, cols
+}
+
+func parseCrates(input []string, rows int, cols int) []stack {
+	crates := make([]stack, cols)
+	for col := 0; col < cols; col++ {
+		for row := rows - 1; row >= 0; row-- {
 			crate := input[row][col*4+1]
 			if string(crate) != " " {
-				crateColumns[col].Push(crate)
+				crates[col].Push(crate)
 			}
 		}
 	}
-	return crateColumns
+	return crates
 }
 
-func moveCrates(input []string, crates *[9]stack) {
+func moveCrates(input []string, crates *[]stack) {
 	for _, v := range input {
 		num, src, dst := 0, 0, 0
 		fmt.Sscanf(v, "move %d from %d to %d", &num, &src, &dst)
 		for j := 0; j < num; j++ {
-			crates[dst-1].Push(crates[src-1].Pop())
+			(*crates)[dst-1].Push((*crates)[src-1].Pop())
 		}
 	}
 }
 
-func moveCrates9001(input []string, crates *[9]stack) {
+func moveCrates9001(input []string, crates *[]stack) {
 	for _, v := range input {
 		num, src, dst := 0, 0, 0
 		fmt.Sscanf(v, "move %d from %d to %d", &num, &src, &dst)
 		// Still cleaner than using slice and index arithmetic directly?
 		var tmp []byte
 		for i := 0; i < num; i++ {
-			tmp = append(tmp, crates[src-1].Pop())
+			tmp = append(tmp, (*crates)[src-1].Pop())
 		}
 		for i := num - 1; i >= 0; i-- {
-			crates[dst-1].Push(tmp[i])
+			(*crates)[dst-1].Push(tmp[i])
 		}
 	}
+}
+
+func getTopCrates(crates []stack) string {
+	result := ""
+	for i := range crates {
+		result += string(crates[i].Pop())
+	}
+	return result
 }
 
 func partOne(input []string) string {
-	crates := parseCrates(input[:8])
-	moveCrates(input[10:], &crates)
-	result := ""
-	for i := range crates {
-		result += string(crates[i][len(crates[i])-1])
-	}
-	return result
+	rows, cols := getMatrixDimensions(input)
+	crates := parseCrates(input[:rows], rows, cols)
+	moveCrates(input[rows+2:], &crates)
+	return getTopCrates(crates)
 }
 
 func partTwo(input []string) string {
-	crates := parseCrates(input[:8])
-	moveCrates9001(input[10:], &crates)
-	result := ""
-	for i := range crates {
-		result += string(crates[i][len(crates[i])-1])
-	}
-	return result
+	rows, cols := getMatrixDimensions(input)
+	crates := parseCrates(input[:rows], rows, cols)
+	moveCrates9001(input[rows+2:], &crates)
+	return getTopCrates(crates)
 }
 
 func main() {
 	input := aoc2022.GetInput(5)
+	fmt.Println(partOne(input))
+	fmt.Println(partTwo(input))
+	input = aoc2022.GetBigBoyInput(5)
 	fmt.Println(partOne(input))
 	fmt.Println(partTwo(input))
 }
