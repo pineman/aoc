ee628bf9a5cfcc709637294dcf53467951464ba9
João Pinheiro <joao.castropinheiro@gmail.com>
2024-12-03 10:08:36 +0000

    d03: steal from andré multiple capture groups

--- 2024/d03.rb
+++ 2024/d03.rb
@@ -3,9 +3,9 @@ input = File.readlines("input/03", chomp: true)
 ex = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"]
 
 def part_one(input)
-  input.flat_map { |l|
-    l.scan(/mul\(([0-9]{1,3},[0-9]{1,3})\)/).map { |m| a, b = m[0].split(",").map(&:to_i); a * b }
-  }.sum
+  input.sum { |l|
+    l.scan(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/).map { |a, b| a.to_i * b.to_i }.sum
+  }
 end
 
 puts part_one(input) # 157621318
@@ -34,3 +34,23 @@ def part_two(input)
 end
 
 puts part_two(input) # 79845780
+
+# https://github.com/andreccosta/advent-of-code/blob/main/2024/day03/part2.rb
+def part_two_stolen(input)
+  t = 0
+  mul = true
+  input.each { |l|
+    l.scan(/(do\(\))|(don't\(\))|mul\(([0-9]{1,3}),([0-9]{1,3})\)/).each { |m_do, m_dont, m_a, m_b|
+      if m_do
+        mul = true
+      elsif m_dont
+        mul = false
+      elsif m_a && m_b
+        t += m_a.to_i * m_b.to_i if mul
+      end
+    }
+  }
+  t
+end
+
+puts part_two_stolen(input) # 79845780
