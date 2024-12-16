9a44d6e1a0611ff6e3a316eababd6f85e7e4b695
João Pinheiro <joao.castropinheiro@gmail.com>
2024-12-16 01:24:47 +0000

    d12: part two wip

--- 2024/d12.rb
+++ 2024/d12.rb
@@ -62,7 +62,46 @@ def part_one(input)
   }.compact.sum
 end
 
-puts part_one(ex)
-puts part_one(ex2)
-puts part_one(ex3)
-puts part_one(input)
+#puts part_one(ex)
+#puts part_one(ex2)
+#puts part_one(ex3)
+#puts part_one(input)
+
+def m(map, shape_set)
+  mi = map.size - 1
+  mj = map[0].size - 1
+  pp shape_set
+  i,j=shape_set.to_a[0]
+  p map[i][j]
+  v_count = 0
+  shape_set.to_a.each { |i, j|
+    v = []
+    v << shape_set.include?([i-1, j]) 
+    v << shape_set.include?([i+1, j]) 
+    v << shape_set.include?([i, j-1])
+    v << shape_set.include?([i, j+1])
+    v_count += 4 if v.count{_1} == 0
+    v_count += 2 if v.count{_1} == 1
+    if v.count{_1} == 2
+      v_count += 2 if !(shape_set.include?([i-1, j]) && shape_set.include?([i+1, j]) || shape_set.include?([i, j-1]) && shape_set.include?([i, j+1]))
+    end
+  }
+  p v_count
+  v_count
+end
+
+def part_two(input)
+  map = input.map(&:chars)
+  v_set = Set.new
+  map.flat_map.with_index { |r, i|
+    r.map.with_index { |c, j|
+      next if v_set.include?([i, j])
+      v_set_bak = v_set.clone
+      area, per = r(map.map(&:clone), v_set, i, j, 0, 0)
+      shape_set = v_set - v_set_bak
+      area * m(map, shape_set)
+    }
+  }.compact.sum
+end
+
+puts part_two(ex)
